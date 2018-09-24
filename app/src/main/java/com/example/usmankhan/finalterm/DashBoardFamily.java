b28a97e56a610a7635abe5416cfae03c0dad76cd
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-24 18:30:50 +0500

    Push all the project file git hub repos

---
+++ app/src/main/java/com/example/usmankhan/finalterm/DashBoardFamily.java
@@ -0,0 +1,13 @@
+package com.example.usmankhan.finalterm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashBoardFamily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_dash_board_family );
+    }
+}
